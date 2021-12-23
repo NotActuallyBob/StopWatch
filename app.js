46ebd4f07c4ae21ddfa7b02b71643f84d7b98d07
Mikael <mikael.virkkunen@aalto.fi>
2021-12-23 20:07:21 +0200

    Merged stop and start button.

--- app.js
+++ app.js
@@ -1,75 +1,77 @@
-let millisecond = 0;
-let sec = 0;
-let min = 0;
+let milliseconds = 0;
+let seconds = 0;
+let minutes = 0;
 let timerStopped = true;
 
 let htmlSeconds;
 let htmlMinutes;
 let htmlMilliseconds;
+let htmlToggleButton;
 
 window.onload = function () {
     htmlMinutes = document.getElementById("minutes");
     htmlSeconds = document.getElementById("seconds");
     htmlMilliseconds = document.getElementById("milliseconds");
 
-    htmlMinutes.innerHTML = "mins";
-    htmlSeconds.innerHTML = "secs";
-    htmlMilliseconds.innerHTML = "milis"
+    htmlToggleButton = document.getElementById("toggleButton");
+
+    htmlMinutes.innerHTML = "00";
+    htmlSeconds.innerHTML = "00";
+    htmlMilliseconds.innerHTML = "00"
 }
 
-function startFunction() {
+function toggleFunction() {
     if(timerStopped){
+        htmlToggleButton.innerHTML = "Stop";
         timerStopped = false;
         timerCycle();
-    }
-}
-
-function stopFunction() {
-    if(!timerStopped){
+    } else {
+        htmlToggleButton.innerHTML = "Resume";
         timerStopped = true;
     }
 }
 
 function resetFunction() {
-    millisecond = 0;
-    sec = 0;
-    min = 0;
-    htmlMilliseconds.innerHTML = "0" + millisecond.toString();
-    htmlSeconds.innerHTML = "0" + sec.toString();
-    htmlMinutes.innerHTML = "0" + min.toString();
+    milliseconds = 0;
+    seconds = 0;
+    minutes = 0;
+    htmlMilliseconds.innerHTML = "0" + milliseconds.toString();
+    htmlSeconds.innerHTML = "0" + seconds.toString();
+    htmlMinutes.innerHTML = "0" + minutes.toString();
+    htmlToggleButton.innerHTML = "Start";
     timerStopped = true;
 }
 
 function timerCycle() {
     if(!timerStopped){
-        millisecond += 1;
+        milliseconds += 1;
 
-        if(millisecond === 100){
-            millisecond = 0;
-            sec += 1;
+        if(milliseconds === 100){
+            milliseconds = 0;
+            seconds += 1;
         }
 
-        if(sec === 60){
-            min += 1;
-            sec = 0;
+        if(seconds === 60){
+            minutes += 1;
+            seconds = 0;
         }
 
-        if(millisecond < 10){
-            htmlMilliseconds.innerHTML = "0" + millisecond.toString();
+        if(milliseconds < 10){
+            htmlMilliseconds.innerHTML = "0" + milliseconds.toString();
         } else {
-            htmlMilliseconds.innerHTML = millisecond.toString();
+            htmlMilliseconds.innerHTML = milliseconds.toString();
         }
 
-        if(sec < 10){
-            htmlSeconds.innerHTML = "0" + sec.toString();
+        if(seconds < 10){
+            htmlSeconds.innerHTML = "0" + seconds.toString();
         } else {
-            htmlSeconds.innerHTML = sec.toString();
+            htmlSeconds.innerHTML = seconds.toString();
         }
 
-        if(min < 10){
-            htmlMinutes.innerHTML = "0" + min.toString();
+        if(minutes < 10){
+            htmlMinutes.innerHTML = "0" + minutes.toString();
         } else {
-            htmlMinutes.innerHTML = min.toString();
+            htmlMinutes.innerHTML = minutes.toString();
         }
 
         setTimeout("timerCycle()", 1);
